f330e0af8c9bcc0b863cb830cd4219716a3a4d2f
Conal Cosgrove <cosgroco@tcd.ie>
2020-08-04 14:52:44 +0100

    https redirect

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const sslRedirect = require('heroku-ssl-redirect');
 const routes = require('./routes/routes.js');
 
 const app = express();
 app.use(express.static('public'));
 app.use(cors())
   .use(cookieParser())
+  .use(sslRedirect())
   .use('/', routes);
 
 const { PORT } = process.env || 8000;
